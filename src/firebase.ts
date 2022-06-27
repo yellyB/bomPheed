667d3a012b78bdf98dbd36f0b1d8cde6f18b1419
bom\bomi <kk3p1q1@gmail.com>
2022-06-27 20:29:42 +0900

    파이어베이스 연결

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa6N7C2Wz1jPLSdWRSpm0WdcDRAM3MV5Y",
+  authDomain: "bompheed.firebaseapp.com",
+  projectId: "bompheed",
+  storageBucket: "bompheed.appspot.com",
+  messagingSenderId: "708919002160",
+  appId: "1:708919002160:web:fd87cb55502ff558fb99f5",
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);

57a0beb7a31fd0c770ee918892d10533692f4930
bom\bomi <kk3p1q1@gmail.com>
2022-06-28 23:11:22 +0900

    router setting

---
+++ src/components/Router.tsx
@@ -0,0 +1,13 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAa6N7C2Wz1jPLSdWRSpm0WdcDRAM3MV5Y",
-  authDomain: "bompheed.firebaseapp.com",
-  projectId: "bompheed",
-  storageBucket: "bompheed.appspot.com",
-  messagingSenderId: "708919002160",
-  appId: "1:708919002160:web:fd87cb55502ff558fb99f5",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase

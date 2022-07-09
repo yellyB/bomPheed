cc95c0ba57cb82db0f4ca438be00b0fda9ad1079
bom\bomi <kk3p1q1@gmail.com>
2022-07-09 22:04:30 +0900

    logout function

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,8 +1,15 @@
 import { User } from "firebase/auth";
 import { useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 interface IProps {
   isLoggedIn: null | User | boolean;
@@ -11,13 +18,18 @@ interface IProps {
 const AppRouter: React.FC<IProps> = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         <Route />
         {isLoggedIn ? (
-          <Route path="/" element={<Home />} />
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route path="/" element={<Auth />} />
         )}
+        {/* <Route path="/*" element={<Navigate to="/"></Navigate>}></Route> */}
       </Routes>
     </Router>
   );
--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,3 +1,18 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const navigation = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigation("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
+};
 
 export default Profile;

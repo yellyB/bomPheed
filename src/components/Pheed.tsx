63cb21599ec92a44e9f139fc93e961941288aeb3
bom\bomi <kk3p1q1@gmail.com>
2022-07-11 21:17:47 +0900

    delete func

--- src/components/Pheed.tsx
+++ src/components/Pheed.tsx
@@ -1,16 +1,27 @@
+import { dbService } from "fbase";
+import { doc } from "@firebase/firestore";
+
 interface IProps {
   pheedObj: any;
   isOwner: boolean;
 }
 
 const Pheed: React.FC<IProps> = ({ pheedObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
+    if (ok) {
+      const data = await doc(dbService, `pheeds/${pheedObj.id}`);
+      console.log(data);
+    }
+  };
+
   return (
     <>
       <div>
         <h4>{pheedObj.text}</h4>
         {isOwner && (
           <>
-            <button>Delete Pheed</button>
+            <button onClick={onDeleteClick}>Delete Pheed</button>
             <button>Edit Pheed</button>
           </>
         )}
